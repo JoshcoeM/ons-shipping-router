6c94646145c978394b6ca87c676e41a7203167da
Josh Moscoe <jmoscoe@manifestcorp.com>
2021-12-21 16:45:39 -0500

    Router sends to oracle db

--- src/main/java/com/manifestcorp/onsshippingrouter/consumer/OnsShippingRouterConsumer.java
+++ src/main/java/com/manifestcorp/onsshippingrouter/consumer/OnsShippingRouterConsumer.java
@@ -1,9 +1,8 @@
-package com.manifestcorp.onsshippingrouter;
+package com.manifestcorp.onsshippingrouter.consumer;
 
 import com.manifestcorp.onsdomain.Order;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.jmx.export.annotation.ManagedOperation;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
@@ -21,7 +20,5 @@ public class OnsShippingRouterConsumer {
     public void listener(Order order){
         kafkaTemplate.send(outputTopic, order);
     }
-
-
 
 }
