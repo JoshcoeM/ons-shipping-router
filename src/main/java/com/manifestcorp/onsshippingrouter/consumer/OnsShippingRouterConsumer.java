f31f3fc89ca1e7b35236bca9656b9473460a51b9
Josh Moscoe <jmoscoe@manifestcorp.com>
2021-12-22 14:42:46 -0500

    added routing

--- src/main/java/com/manifestcorp/onsshippingrouter/consumer/OnsShippingRouterConsumer.java
+++ src/main/java/com/manifestcorp/onsshippingrouter/consumer/OnsShippingRouterConsumer.java
@@ -5,20 +5,31 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class OnsShippingRouterConsumer {
 
-    @Value("${k.topic.producer.name}")
-    String outputTopic;
+    @Value("${k.topic.mainDc.producer.name}")
+    String mainDcTopic;
+
+    @Value("${k.topic.ohioDc.producer.name}")
+    String ohioDcTopic;
+
 
     @Autowired
     private KafkaTemplate kafkaTemplate;
 
     @KafkaListener(topics = "${k.topic.consumer.name}")
-    public void listener(Order order){
-        kafkaTemplate.send(outputTopic, order);
+
+    public void listener(@Payload Order order, @Header("DistributionCenter") String distributionCenter){
+        if(distributionCenter.equals("ohio-dc")){
+            kafkaTemplate.send(ohioDcTopic, order);
+        }else if(distributionCenter.equals("main-dc")){
+            kafkaTemplate.send(mainDcTopic, order);
+        }
     }
 
 }
